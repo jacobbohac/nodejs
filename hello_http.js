ffcb57d189f262e12a127e1cef85a72515b1a69a
jbohac <jacob.bohac@gmail.com>
2016-05-26 20:45:04 -0500

    simple update

--- hello_http.js
+++ hello_http.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
 	res.writeHead(200);
-	res.end('Hello Http');
+	res.end('Hello!\nYour server is still alive');
 });
 
 server.listen(8080);
