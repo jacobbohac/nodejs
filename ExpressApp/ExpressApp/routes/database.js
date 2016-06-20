5bc81b2a5944d27593ca78515fb0640f8f8c7378
Jacob Bohac <Jacob Bohac>
2016-06-20 00:53:00 -0500

    added vs express example

---
+++ ExpressApp/ExpressApp/routes/database.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET database page. */
+router.get('/', function (req, res) {
+    res.render('database', { title: 'Express' });
+});
+
+module.exports = router;
--- hello_http.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-var server = http.createServer(function(req, res) {
-	res.writeHead(200);
-	res.end('Hello!\nYour server is still alive');
-});
-
-server.listen(8080);
